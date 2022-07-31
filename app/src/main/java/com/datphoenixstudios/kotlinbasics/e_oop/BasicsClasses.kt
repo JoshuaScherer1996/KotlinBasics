2a7ceb932ffc4b7cd578f7dbbd8bc363231dc9e4
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2022-07-31 18:46:06 +0200

    done with constructors and initializers

--- app/src/main/java/com/datphoenixstudios/kotlinbasics/e_oop/BasicsClasses.kt
+++ app/src/main/java/com/datphoenixstudios/kotlinbasics/e_oop/BasicsClasses.kt
@@ -1,3 +1,33 @@
 package com.datphoenixstudios.kotlinbasics.e_oop
 
-//Placeholder strikes again
+fun main() {
+    //creating a new object with our blueprint class
+    //a class is also a datatype
+    var appDev: Person = Person("Joshua", "Scherer")
+    //here the person is autmatically created with out default values
+    var john = Person()
+    //creating an object with only the last name known
+    var mrPeterson = Person(lastName = "Peterson")
+
+}
+
+//structure of a class: keyword class - name of class - keyword constructor or just round brackets
+// - (attributes) - {Codeblock of the class}
+
+//with keyword constructor
+/*
+class Person constructor(firstName: String, lastName: String) {
+
+}
+*/
+
+//without keyword constructor but therefore with default values
+class Person(firstName: String = "John", lastName: String = "Doe") {
+
+    //initializers. Runs automatically when an object is created.
+    //so our appDev automatically will run this code when created
+    init {
+        println("App Developer successfully created! First name is $firstName and last name is $lastName")
+    }
+
+}
