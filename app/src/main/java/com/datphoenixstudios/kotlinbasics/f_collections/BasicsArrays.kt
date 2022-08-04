ce2aa91c8d2bac738e40e271614ff16d830b3d87
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2022-08-04 22:52:55 +0200

    quicksafe arrays

--- app/src/main/java/com/datphoenixstudios/kotlinbasics/f_collections/BasicsArrays.kt
+++ app/src/main/java/com/datphoenixstudios/kotlinbasics/f_collections/BasicsArrays.kt
@@ -1,3 +1,14 @@
 package com.datphoenixstudios.kotlinbasics.f_collections
 
-//Placeholder commit
+fun main() {
+    //Array of integers specifically using the proper Datatype
+    //val number: IntArray = intArrayOf(1,2,3,4,5,6)
+
+    //code automatically detects the type of our variable via type inferrence
+    //val number = intArrayOf(1,2,3,4,5,6)
+
+    //again also sees that it is an int array in an even shorter form
+    val number = arrayOf(1,2,3,4,5,6)
+
+    println(number)
+}
